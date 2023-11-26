4d85e51c02bf12d661db3ed09d2c7baa5b1c169c
alexjuv <agj06@mail.aub.edu>
2023-11-26 23:10:14 +0200

    flask stuff

--- shipswift/app.py
+++ shipswift/app.py
@@ -1,16 +1,13 @@
-# app.py
-from flask import Flask
+from flask import Flask, request, jsonify
+import mysql.connector
+
 
 app = Flask(__name__)
 
-# Import and register blueprints (routes) for each module
-from customers.routes import customers_bp
-from inventory.routes import inventory_bp
-from sales.routes import sales_bp
-
-app.register_blueprint(customers_bp, url_prefix='/customers')
-app.register_blueprint(inventory_bp, url_prefix='/inventory')
-app.register_blueprint(sales_bp, url_prefix='/sales')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="A.lexjuv123",
+    database="DB"
+)
+my_cursor = mydb.cursor()
